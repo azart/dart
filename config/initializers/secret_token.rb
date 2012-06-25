144e1ddd2631a351773801e10d96de62c50340e7
AFarforov <anton@black-sheep.ru>
2012-06-25 16:05:22 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dart::Application.config.secret_token = '3d06f060c0dcd55a395cf3c35b26f7285c78d701b8eb99a60af16bb69f092a819288b5072f86b5cdc59512c925d8d71b3db40a92c44dd83cf4bb75218a7d8099'
